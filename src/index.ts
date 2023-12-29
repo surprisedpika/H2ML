b1f8c78a6baa7b72015893723a8434b3e88c9189
Surprised <surprisedpikaaaaaaa@gmail.com>
2023-12-29 21:50:00 +0000

    Parse variables in text and <@var> tag

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import * as htmlparser2 from "htmlparser2";
-import { defaultCompilerOptions, type CompilerOptions } from "./types";
+import { defaultCompilerOptions, type CompilerOptions, type Console } from "./types";
+import { Parser } from "expr-eval";
 const test =
-  "<html><@repeat class='something' count=2><p>slay</p><@repeat count=5><p>hello</p></@repeat></@repeat><div></div></html><!-- comment -->";
+  "<html><@repeat class='something' count=2><@var x='4' y='5'></@var><p>{x}</p><@repeat count=5><p>hello</p><@var x='7'></@var></@repeat>{x}</@repeat><div></div></html><!-- comment -->";
 
 const mapAttributes = (attributes: { [s: string]: string }) => {
   return Object.entries(attributes)
@@ -9,21 +10,36 @@ const mapAttributes = (attributes: { [s: string]: string }) => {
     .join(" ");
 };
 
+const replaceVariables = (
+  input: string,
+  variables: { [key: string]: string }
+) => {
+  return input.replace(/\{([^{}]+)\}/g, (_match, content) => {
+    const p = new Parser();
+    const expression = p.parse(content);
+    return expression.evaluate(variables);
+  })
+};
+
 function compile(input: string, opts: CompilerOptions) {
   const options = { ...defaultCompilerOptions, ...opts };
 
-  const c = {
+  const c: Console = {
     error: (...data: any[]) => options.logErrors && console.error(...data),
     warn: (...data: any[]) => options.logWarnings && console.warn(...data),
     log: (...data: any[]) => options.verbose && console.warn(...data),
   };
 
   let out = "";
+  let variables: { [key: string]: string } = {};
 
   const parser = new htmlparser2.Parser(
     {
       onparserinit() {
         c.log("Parser Initialised...");
+      },
+      onend() {
+        c.log("Parser Finished.");
       },
       onerror(error) {
         c.error(error);
@@ -34,16 +50,37 @@ function compile(input: string, opts: CompilerOptions) {
         }
       },
       onopentag(name, attribs, _isImplied) {
-        const attributes = mapAttributes(attribs);
-        const joiner = attributes ? " " : "";
-        out += `<${name}${joiner}${attributes}>`
+        if (name.startsWith("@")) {
+          switch (name) {
+            case "@var":
+              Object.entries(attribs).map(([key, value]) => {
+                variables[key] = value;
+                c.log("Variable", key, "set to", value);
+              });
+              break;
+            default:
+              c.warn("Unknown H2ML tag", name, "with attributes:", attribs);
+              break;
+          }
+        } else {
+          const attributes = mapAttributes(attribs);
+          const joiner = attributes ? " " : "";
+          out += `<${name}${joiner}${attributes}>`;
+        }
       },
       ontext(data) {
-        out += data;
+        out += replaceVariables(data, variables);
       },
       onclosetag(name, _isImplied) {
-        out += `</${name}>`
-      }
+        switch (name) {
+          case "@var":
+          case "@repeat":
+            break;
+          default:
+            out += `</${name}>`;
+            break;
+        }
+      },
     },
     { xmlMode: true, lowerCaseTags: true, lowerCaseAttributeNames: true }
   );
@@ -53,4 +90,11 @@ function compile(input: string, opts: CompilerOptions) {
 }
 
 console.log(test);
-console.log(compile(test, { logErrors: true, logWarnings: true, verbose: true, preserveComments: true }));
+console.log(
+  compile(test, {
+    logErrors: true,
+    logWarnings: true,
+    verbose: true,
+    preserveComments: true,
+  })
+);
--- src/types.ts
+++ src/types.ts
@@ -3,11 +3,17 @@ export type CompilerOptions = {
   logErrors?: boolean;
   verbose?: boolean;
   preserveComments?: boolean;
-}
+};
 
 export const defaultCompilerOptions: Required<CompilerOptions> = {
   logWarnings: true,
   logErrors: true,
   verbose: false,
-  preserveComments: false
-}
+  preserveComments: false,
+};
+
+export type Console = {
+  error: (...data: any[]) => false | void;
+  warn: (...data: any[]) => false | void;
+  log: (...data: any[]) => false | void;
+};
